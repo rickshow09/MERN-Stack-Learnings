bd72e17d43e9bc364b0318857326cb9dc6a5d4d6
rickshow09 <rickraghavshow@gamil.com>
2026-08-24 12:42:56 +0530

    Positive, Negative, or Zero Program

---
+++ Problems/Conditional_Statements/PositiveNegativeOrZero.js
@@ -0,0 +1,13 @@
+// Positive, Negative, or Zero
+// Check if a given number is positive, negative, or zero.
+
+var num = 9;
+
+if (num > 0) {
+    console.log(`${num} is positive..!`);
+}
+else if (num < 0) {
+    console.log(`${num} is negative..!`);
+} else {
+    console.log(`${num} is zero..!`);
+}
