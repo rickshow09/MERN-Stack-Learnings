54be1cf328aab097e6d893c850f9303e8d4c05ad
rickshow09 <rickraghavshow@gamil.com>
2026-08-24 16:56:23 +0530

    Eligible to Vote Program

---
+++ Problems/Conditional_Statements/EligibleToVote.js
@@ -0,0 +1,7 @@
+// Eligible to Vote
+// Check if a person is eligible to vote based on their age (age ≥ 18).
+
+var age = 17;
+
+console.log(age >= 18 ? "You are eligible to vote..!" : "Sorry, you are not eligible to vote..!");
+
