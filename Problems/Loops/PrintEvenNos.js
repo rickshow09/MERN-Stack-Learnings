58eacca9f1f06541c690572990c4bc54ba64a3b6
rickshow09 <rickraghavshow@gamil.com>
2026-08-24 18:38:33 +0530

    Print Even Numbers Program

---
+++ Problems/Loops/PrintEvenNos.js
@@ -0,0 +1,11 @@
+// Print Even Numbers
+// Print all even numbers from 1 to 50 using a loop.
+
+
+
+for (let index = 1; index <= 50; index++) {
+    if (index % 2 == 0) {
+        console.log(`${index} is even number.`);
+    }
+}
+
