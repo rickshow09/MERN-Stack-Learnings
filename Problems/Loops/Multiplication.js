79de9bb05e81641aeb4165818415e87e7849f6b1
rickshow09 <rickraghavshow@gamil.com>
2026-08-24 11:46:11 +0530

    Multiplication Program

---
+++ Problems/Loops/Multiplication.js
@@ -0,0 +1,8 @@
+// Multiplication Table
+// Write a program to print the multiplication table of a given number using a loop.
+
+var num = 2;
+
+for (let index = 1; index <= 10; index++) {
+    console.log(`${num} * ${index} = ${num * index}`);
+}
