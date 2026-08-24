bc48923e76d0cec47d10727454583cc1c5e34a62
rickshow09 <rickraghavshow@gamil.com>
2026-08-24 17:59:47 +0530

    Sum of Natural Numbers Program

---
+++ Problems/Loops/SumOfNaturalNos.js
@@ -0,0 +1,11 @@
+// Sum of Natural Numbers
+// Find the sum of the first n natural numbers using a loop.
+
+var num = 59;
+var sum = 0;
+
+for (let index = 0; index <= num; index++) {
+    sum = sum + index;    
+}
+
+console.log(`Sum of First ${num} natural numbers is ${sum}`);
