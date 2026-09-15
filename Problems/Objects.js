aa46bd611dadcf3b66ac3dd016abf2d1575e3653
rickshow09 <rickraghavshow@gamil.com>
2026-09-15 08:26:32 +0530

    Objects Program

---
+++ Problems/Objects.js
@@ -0,0 +1,22 @@
+//Literal Approach
+
+let person = {
+    fanme: 'Rick',
+    lname: 'Show',
+    age: 24
+    // address: {
+    //     city: 'Mumbai'
+    // }
+}
+
+// console.log(person.fanme);
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+console.log(Object.entries(person));
+//console.log(person.address.city);
+
+
+
+//Constructor Approach
+
+let person1 = new Object();
