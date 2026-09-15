72930e8caa589f0476a269652b8509eff6c23ac6
rickshow09 <rickraghavshow@gamil.com>
2026-09-15 08:35:16 +0530

    Printing Prime Numbers using For Loop

---
+++ Problems/Loops/PrimeNos.js
@@ -0,0 +1,12 @@
+// Check Prime Number
+// Check if a given number is prime using a loop.
+
+var num = 100;
+
+console.log(`Printing Prime Numbers From 0 to ${num}`)
+
+for (var i = 0; i <= num; i++) {
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
